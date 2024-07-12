4dfda729b16907746923fb4e3603c5b3de3709b0
tuke <jrriwll@gmail.com>
2024-07-12 23:22:41 +0800

    commit

--- compose.oss/dataease/install.sh
+++ compose.oss/dataease/install.sh
@@ -3,3 +3,12 @@
 mkdir conf data logs etcd_data apisix/logs
 
 docker-compose up -d
+# uid 1001 for etcd
+sudo chown -R "1001:$(id -g -n)" etcd
+
+#character_set_server=utf8
+#lower_case_table_names=1
+#group_concat_max_len=1024000
+docker exec -it mysql mysql -uroot -p -e "
+CREATE DATABASE IF NOT EXISTS dataease DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+"
--- compose/dev/install.sh
+++ compose/dev/install.sh
@@ -7,7 +7,10 @@ docker-compose up -d
 # uid 1001 for kafka
 sudo chown -R "1001:$(id -g -n)" kafka37
 
+docker-compose ps -a
+
 LOCAL_IP=
 sed -i "s/\${LOCAL_IP}/$LOCAL_IP/g" nginx/dev.dreamcat.org.conf
 # docker exec -it nginx nginx -s reload
-docker exec -it nginx nginx -t
+
+(docker ps --format 'table {{.Names}}' | grep nginx) && docker exec -it nginx nginx -t
