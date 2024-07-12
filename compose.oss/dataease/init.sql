02508ae6df7ae7dbaaab4ecd9b4bb9feeecb26ce
tuke <jrriwll@gmail.com>
2024-07-12 23:56:03 +0800

    commit

--- compose.oss/dataease/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE `dataease` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-
-/*
-need mysql:8.3
-
-character_set_server=utf8
-lower_case_table_names=1
-group_concat_max_len=1024000
-*/
