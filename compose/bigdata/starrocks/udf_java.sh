135efb8cf298f4ee51d18d84fa8cda018a9b3f54
tuke <jrriwll@gmail.com>
2026-03-08 22:29:04 +0800

    commit

---
+++ compose/bigdata/starrocks/udf_java.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# copy JDK to BE
+docker run -itd --name starrocks-jdk-tmp jerrywill/jdk:17 bash
+
+
+JDK_DIR=$(docker exec starrocks-jdk-tmp bash -c 'echo $JAVA_HOME | cut -d/ -f3')
+
+if [[ -z "$jdk_dir" ]]; then
+  echo "failed to find JDK_DIR"
+  exit 1
+fi
+
+docker cp starrocks-jdk-tmp:/opt/$JDK_DIR .
+
+for i in starrocks-be1 starrocks-be2 starrocks-be3; do
+    docker cp $JDK_DIR $i:/opt/
+done
+
+cat <<EOF >> conf/be.conf
+JAVA_HOME = "/opt/$JDK_DIR"
+EOF
+
+docker restart be1 be2 be3
